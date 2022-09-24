472db52fa061eed572dd65118f958b6df8daf6f9
Terry <tshe557@aucklanduni.ac.nz>
2022-09-24 21:22:01 +1200

    Now all user profile interactions are managed by the class UserProfileManager

--- src/main/java/nz/ac/auckland/se206/ChooseProfileController.java
+++ src/main/java/nz/ac/auckland/se206/ChooseProfileController.java
@@ -2,87 +2,72 @@ package nz.ac.auckland.se206;
 
 import java.io.IOException;
 import java.net.URISyntaxException;
-
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 
 public class ChooseProfileController {
 
-	@FXML
-	private Button leftButton;
+  @FXML private Button leftButton;
 
-	@FXML
-	private Button rightButton;
+  @FXML private Button rightButton;
 
-	@FXML
-	private Button deleteUserButton;
+  @FXML private Button deleteUserButton;
 
-	@FXML
-	private Button addUserButton;
+  @FXML private Button addUserButton;
 
-	@FXML
-	private ImageView profileImage;
+  @FXML private ImageView profileImage;
 
-	/**
-	 * JavaFX calls this method once the GUI elements are loaded.
-	 * 
-	 * @throws URISyntaxException
-	 * @throws IOException
-	 */
-	public void initialize() throws IOException, URISyntaxException {
+  @FXML private Label userNameLabel;
 
-		// Load the selected user image
-		Image userProfile = new Image(this.getClass().getResource("/images/profileImages/0.jpg").toString());
-		profileImage.setImage(userProfile);
+  /**
+   * JavaFX calls this method once the GUI elements are loaded.
+   *
+   * @throws URISyntaxException
+   * @throws IOException
+   */
+  public void initialize() throws IOException, URISyntaxException {
 
-	}
+    // Load the selected user image
+    Image userProfile =
+        new Image(this.getClass().getResource("/images/profileImages/0.jpg").toString());
+    profileImage.setImage(userProfile);
+  }
 
-	/**
-	 * This method is invoked when the user clicks on the delete button of the
-	 * select user profile.
-	 */
-	@FXML
-	private void onDeleteUser() {
+  /**
+   * This method is invoked when the user clicks on the delete button of the select user profile.
+   */
+  @FXML
+  private void onDeleteUser() {}
 
-	}
+  /** This method is invoked when the user clicks on the add button to add a new user. */
+  @FXML
+  private void onAddUser(ActionEvent event) {
 
-	/**
-	 * This method is invoked when the user clicks on the add button to add a new
-	 * user.
-	 */
-	@FXML
-	private void onAddUser(ActionEvent event) {
+    Button button = (Button) event.getSource();
+    Scene sceneButtonIsIn = button.getScene();
 
-		Button button = (Button) event.getSource();
-		Scene sceneButtonIsIn = button.getScene();
+    try {
+      sceneButtonIsIn.setRoot(App.loadFxml("create_profile"));
 
-		try {
-			sceneButtonIsIn.setRoot(App.loadFxml("create_profile"));
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
+  }
 
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
+  /** This method is invoked when the user clicks on the left arrow. */
+  @FXML
+  private void onGoLeft() {
+    System.out.println("Go left");
+  }
 
-	}
-
-	/**
-	 * This method is invoked when the user clicks on the left arrow.
-	 */
-	@FXML
-	private void onGoLeft() {
-		System.out.println("Go left");
-	}
-
-	/**
-	 * This method is invoked when the user clicks on the right arrow.
-	 */
-	@FXML
-	private void onGoRight() {
-		System.out.println("Go right");
-
-	}
+  /** This method is invoked when the user clicks on the right arrow. */
+  @FXML
+  private void onGoRight() {
+    System.out.println("Go right");
+  }
 }
