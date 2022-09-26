74bd17b34c44faeac9cd20d1eaff8b0bca7d5d70
lia-arroyo <liayzabel@gmail.com>
2022-09-26 18:09:27 +1300

    added a progress bar to showcase how much progress the user has had so far

--- src/main/java/nz/ac/auckland/se206/StatsController.java
+++ src/main/java/nz/ac/auckland/se206/StatsController.java
@@ -1,25 +1,30 @@
 package nz.ac.auckland.se206;
 
+import com.opencsv.exceptions.CsvException;
 import java.io.IOException;
+import java.net.URISyntaxException;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.ProgressBar;
 import javafx.scene.control.ScrollPane;
 import javafx.scene.image.ImageView;
 import javafx.scene.text.Font;
 import nz.ac.auckland.se206.profiles.UserProfile;
 import nz.ac.auckland.se206.profiles.UserProfileManager;
+import nz.ac.auckland.se206.words.CategorySelector;
 
 public class StatsController {
   @FXML private Label statsTitleLabel;
   @FXML private Label winsLabel;
   @FXML private Label lossesLabel;
   @FXML private Label highestPredictionLabel;
-  @FXML private Label wordHistoryLabel;
+  @FXML private Label progressLabel;
   @FXML private ImageView userProfileImage;
   @FXML private ScrollPane wordHistoryScrollPane;
+  @FXML private ProgressBar wordProgressBar;
 
   private UserProfile currentProfile = UserProfileManager.currentProfile;
 
@@ -36,31 +41,59 @@ public class StatsController {
    * it on screen.
    */
   public void updateStatistics() {
-    // updating win count
+    // updating win and loss counts
     winsLabel.setText(currentProfile.getWinsCount() + "");
+    lossesLabel.setText(currentProfile.getLossesCount() + "");
 
     // updating the highest prediction
     highestPredictionLabel.setText(Math.round(currentProfile.getHighestPrediction() * 100) + "%");
 
-    // updating loss count
-    lossesLabel.setText(currentProfile.getLossesCount() + "");
-
-    // updating word history
+    // updating the word history and progress
     displayWordHistory();
+    updateProgress();
   }
 
   /** This method will display the word history using a String Builder. */
   public void displayWordHistory() {
     StringBuilder builder = new StringBuilder();
 
+    // listing every word that the user has played.
     for (String word : currentProfile.getWordHistory()) {
       builder.append(word + "\n");
     }
 
+    // creating new label that resizes depending on length of above string
     Label words = new Label(builder.toString());
     words.setFont(new Font(38));
 
+    // adding to the scroll pane
     wordHistoryScrollPane.setContent(words);
+  }
+
+  public void updateProgress() {
+    // Creating new category selector to get the number of total words
+    CategorySelector categorySelector = null;
+    try {
+      categorySelector = new CategorySelector();
+    } catch (IOException | RuntimeException | URISyntaxException | CsvException e) {
+      throw new RuntimeException(e);
+    }
+
+    // calculating progress
+    int wordsPlayed = currentProfile.getWordHistory().size();
+    int wordsInTotal = categorySelector.getTotalWordCount(CategorySelector.Difficulty.E);
+    double progress = (double) wordsPlayed / wordsInTotal;
+
+    // updating the progress bar
+    wordProgressBar.setProgress(progress);
+
+    // updating the text that goes with it
+    progressLabel.setText(
+        "You've played a total of "
+            + wordsPlayed
+            + " words, and only have "
+            + (wordsInTotal - wordsPlayed)
+            + " words to go! Well done!");
   }
 
   /**
