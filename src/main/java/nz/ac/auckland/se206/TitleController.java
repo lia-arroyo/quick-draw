40b36a1cb5a07449ca595823fe72e19db41c79d3
Dana <mseo447@aucklanduni.ac.nz>
2022-09-27 01:38:30 +1300

    Ran the format command to do final formatting on the code.

--- src/main/java/nz/ac/auckland/se206/TitleController.java
+++ src/main/java/nz/ac/auckland/se206/TitleController.java
@@ -4,7 +4,6 @@ import java.io.File;
 import java.io.IOException;
 import java.util.Timer;
 import java.util.TimerTask;
-
 import javafx.fxml.FXML;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
@@ -12,76 +11,75 @@ import javafx.scene.layout.Pane;
 
 public class TitleController {
 
-	@FXML
-	private Pane pane;
+  @FXML private Pane pane;
 
-	@FXML
-	private ImageView titleImage;
+  @FXML private ImageView titleImage;
 
-	private Boolean noProfiles = false;
-	private int imageIndex = 0;
+  private Boolean noProfiles = false;
+  private int imageIndex = 0;
 
-	public void initialize() throws IOException {
+  public void initialize() throws IOException {
 
-		File profiles = new File("./src/main/resources/profiles/profiles.json");
+    File profiles = new File("./src/main/resources/profiles/profiles.json");
 
-		if (!profiles.exists()) {
-			profiles.createNewFile();
-			noProfiles = true;
+    if (!profiles.exists()) {
+      profiles.createNewFile();
+      noProfiles = true;
 
-		} else {
-			if (profiles.length() == 0) {
-				noProfiles = true;
-			}
-		}
+    } else {
+      if (profiles.length() == 0) {
+        noProfiles = true;
+      }
+    }
 
-		Timer timer = new Timer();
+    Timer timer = new Timer();
 
-		timer.schedule(new TimerTask() {
-			@Override
-			public void run() {
+    timer.schedule(
+        new TimerTask() {
+          @Override
+          public void run() {
 
-				if (imageIndex < 5) {
+            if (imageIndex < 5) {
 
-					Image image = new Image(this.getClass()
-							.getResource(String.format("/images/titleImages/%d.PNG", imageIndex)).toString());
-					titleImage.setImage(image);
-					imageIndex++;
-				} else {
+              Image image =
+                  new Image(
+                      this.getClass()
+                          .getResource(String.format("/images/titleImages/%d.PNG", imageIndex))
+                          .toString());
+              titleImage.setImage(image);
+              imageIndex++;
+            } else {
 
-					if (noProfiles) {
-						moveToCreateProfile();
+              if (noProfiles) {
+                moveToCreateProfile();
 
-					} else {
-						moveToChooseProfile();
-					}
+              } else {
+                moveToChooseProfile();
+              }
 
-					timer.cancel();
+              timer.cancel();
+            }
+          }
+        },
+        0,
+        800);
+  }
 
-				}
+  private void moveToCreateProfile() {
+    try {
+      pane.getScene().setRoot(App.loadFxml("create_profile"));
 
-			}
-		}, 0, 800);
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
+  }
 
-	}
+  private void moveToChooseProfile() {
+    try {
+      pane.getScene().setRoot(App.loadFxml("choose_profile"));
 
-	private void moveToCreateProfile() {
-		try {
-			pane.getScene().setRoot(App.loadFxml("create_profile"));
-
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-	}
-
-	private void moveToChooseProfile() {
-		try {
-			pane.getScene().setRoot(App.loadFxml("choose_profile"));
-
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-
-	}
-
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
+  }
 }
