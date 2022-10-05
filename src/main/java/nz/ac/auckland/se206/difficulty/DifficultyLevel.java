967ee69f7b9237fc6610b54147113ad22a60e6ba
Terry <tshe557@aucklanduni.ac.nz>
2022-10-06 09:56:33 +1300

    Added new class - DifficultyLevel, which allows diffulty level settings for users to be stored

---
+++ src/main/java/nz/ac/auckland/se206/difficulty/DifficultyLevel.java
@@ -0,0 +1,52 @@
+package nz.ac.auckland.se206.difficulty;
+
+public class DifficultyLevel {
+
+	public enum Accuracy {
+		E, M, H
+	}
+
+	public enum Words {
+		E, M, H
+	}
+
+	public enum Time {
+		E, M, H
+	}
+
+	public enum Confidence {
+		E, M, H
+	}
+
+	private Accuracy accuracyLevel;
+
+	private Words wordsLevel;
+
+	private Time timeLevel;
+
+	private Confidence confidenceLevel;
+
+	public DifficultyLevel() {
+		this.accuracyLevel = Accuracy.E;
+		this.wordsLevel = Words.E;
+		this.timeLevel = Time.E;
+		this.confidenceLevel = Confidence.E;
+	}
+
+	private void setAccuracyLevel(Accuracy accuracyLevel) {
+		this.accuracyLevel = accuracyLevel;
+	}
+
+	private void setWordsLevel(Words wordsLevel) {
+		this.wordsLevel = wordsLevel;
+	}
+
+	private void setTimeLevel(Time timeLevel) {
+		this.timeLevel = timeLevel;
+	}
+
+	private void setConfidenceLevel(Confidence confidenceLevel) {
+		this.confidenceLevel = confidenceLevel;
+	}
+
+}
