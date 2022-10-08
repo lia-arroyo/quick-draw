455d412589c503f8e4c2f0388b14f7bec1c3fcd8
Terry <tshe557@aucklanduni.ac.nz>
2022-10-08 22:04:39 +1300

    Created settings page where user can choose the game difficulty settings and a controller for the settings page

---
+++ src/main/java/nz/ac/auckland/se206/SettingsController.java
@@ -0,0 +1,20 @@
+package nz.ac.auckland.se206;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Slider;
+
+public class SettingsController {
+
+	@FXML
+	private Slider accuracySlider;
+
+	@FXML
+	private Slider wordsSlider;
+
+	@FXML
+	private Slider timeSlider;
+
+	@FXML
+	private Slider confidenceSlider;
+
+}
