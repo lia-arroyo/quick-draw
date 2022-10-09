3da72cc894fdc07fe532c6f1019e996d45ea564a
Terry <tshe557@aucklanduni.ac.nz>
2022-10-09 15:43:32 +1300

    Initialised slider labels in Settings page to display levels of difficulty for each slider and the current level of difficulty for each slider

--- src/main/java/nz/ac/auckland/se206/SettingsController.java
+++ src/main/java/nz/ac/auckland/se206/SettingsController.java
@@ -2,6 +2,8 @@ package nz.ac.auckland.se206;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Slider;
+import javafx.util.StringConverter;
+import nz.ac.auckland.se206.difficulty.DifficultyLevel;
 
 public class SettingsController {
 
@@ -16,5 +18,49 @@ public class SettingsController {
 
 	@FXML
 	private Slider confidenceSlider;
+
+	public void initialize() {
+		String[] currentDifficulties = new String[4];
+		currentDifficulties[0] = DifficultyLevel.currentAccuracyLevel.toString();
+		currentDifficulties[1] = DifficultyLevel.currentWordsLevel.toString();
+		currentDifficulties[2] = DifficultyLevel.currentTimeLevel.toString();
+		currentDifficulties[3] = DifficultyLevel.currentConfidenceLevel.toString();
+
+		Slider[] difficultySliders = new Slider[4];
+		difficultySliders[0] = this.accuracySlider;
+		difficultySliders[1] = this.wordsSlider;
+		difficultySliders[2] = this.timeSlider;
+		difficultySliders[3] = this.confidenceSlider;
+
+		for (int i = 0; i < 4; i++) {
+			if (currentDifficulties[i] == "E") {
+				difficultySliders[i].setValue((int) 0);
+			} else if (currentDifficulties[i] == "M") {
+				difficultySliders[i].setValue((int) 1);
+			} else if (currentDifficulties[i] == "H") {
+				difficultySliders[i].setValue((int) 2);
+			}
+		}
+
+		for (int i = 0; i < 4; i++) {
+			difficultySliders[i].setLabelFormatter(new StringConverter<Double>() {
+				@Override
+				public String toString(Double number) {
+					if (number == 0) {
+						return "Easy";
+					} else if (number == 1) {
+						return "Medium";
+					} else {
+						return "Hard";
+					}
+				}
+
+				@Override
+				public Double fromString(String string) {
+					return null;
+				}
+			});
+		}
+	}
 
 }
