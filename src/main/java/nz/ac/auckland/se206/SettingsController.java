42fa6a6402502d36aac29fcbc9aafd597590d9af
Terry <tshe557@aucklanduni.ac.nz>
2022-10-09 18:58:30 +1300

    Added temporary buttons to switch between the main menu page and the settings page, fixed bug in the settings page regarding updating user settings with slider, user settings now auto-saved each time the slider position is moved for each difficulty

--- src/main/java/nz/ac/auckland/se206/SettingsController.java
+++ src/main/java/nz/ac/auckland/se206/SettingsController.java
@@ -1,8 +1,12 @@
 package nz.ac.auckland.se206;
 
+import java.io.IOException;
 import javafx.beans.value.ChangeListener;
 import javafx.beans.value.ObservableValue;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.Slider;
 import javafx.util.StringConverter;
 import nz.ac.auckland.se206.difficulty.DifficultyLevel;
@@ -14,136 +18,190 @@ import nz.ac.auckland.se206.profiles.UserProfileManager;
 
 public class SettingsController {
 
-	@FXML
-	private Slider accuracySlider;
+  @FXML private Slider accuracySlider;
 
-	@FXML
-	private Slider wordsSlider;
+  @FXML private Slider wordsSlider;
 
-	@FXML
-	private Slider timeSlider;
+  @FXML private Slider timeSlider;
 
-	@FXML
-	private Slider confidenceSlider;
+  @FXML private Slider confidenceSlider;
 
-	private static void updateUserAccuracyDifficulty(Accuracy accuracyLevel) {
-		UserProfileManager.userProfileList.get(UserProfileManager.currentProfileIndex).getDifficultyLevel()
-				.setAccuracyLevel(accuracyLevel);
-	}
+  private static void updateUserAccuracyDifficulty(Accuracy accuracyLevel) {
+    UserProfileManager.userProfileList
+        .get(UserProfileManager.currentProfileIndex)
+        .getDifficultyLevel()
+        .setAccuracyLevel(accuracyLevel);
+    UserProfileManager.saveToFile();
+  }
 
-	private static void updateUserWordsDifficulty(Words wordsLevel) {
-		UserProfileManager.userProfileList.get(UserProfileManager.currentProfileIndex).getDifficultyLevel()
-				.setWordsLevel(wordsLevel);
-	}
+  private static void updateUserWordsDifficulty(Words wordsLevel) {
+    UserProfileManager.userProfileList
+        .get(UserProfileManager.currentProfileIndex)
+        .getDifficultyLevel()
+        .setWordsLevel(wordsLevel);
+    UserProfileManager.saveToFile();
+  }
 
-	private static void updateUserTimeDifficulty(Time timeLevel) {
-		UserProfileManager.userProfileList.get(UserProfileManager.currentProfileIndex).getDifficultyLevel()
-				.setTimeLevel(timeLevel);
-	}
+  private static void updateUserTimeDifficulty(Time timeLevel) {
+    UserProfileManager.userProfileList
+        .get(UserProfileManager.currentProfileIndex)
+        .getDifficultyLevel()
+        .setTimeLevel(timeLevel);
+    UserProfileManager.saveToFile();
+  }
 
-	private static void updateUserConfidenceDifficulty(Confidence confidenceLevel) {
-		UserProfileManager.userProfileList.get(UserProfileManager.currentProfileIndex).getDifficultyLevel()
-				.setConfidenceLevel(confidenceLevel);
-	}
+  private static void updateUserConfidenceDifficulty(Confidence confidenceLevel) {
+    UserProfileManager.userProfileList
+        .get(UserProfileManager.currentProfileIndex)
+        .getDifficultyLevel()
+        .setConfidenceLevel(confidenceLevel);
+    UserProfileManager.saveToFile();
+  }
 
-	public void initialize() {
-		String[] currentDifficulties = new String[4];
-		currentDifficulties[0] = UserProfileManager.userProfileList.get(UserProfileManager.currentProfileIndex)
-				.getDifficultyLevel().getAccuracyLevel().toString();
-		currentDifficulties[1] = UserProfileManager.userProfileList.get(UserProfileManager.currentProfileIndex)
-				.getDifficultyLevel().getWordsLevel().toString();
-		currentDifficulties[2] = UserProfileManager.userProfileList.get(UserProfileManager.currentProfileIndex)
-				.getDifficultyLevel().getTimeLevel().toString();
-		currentDifficulties[3] = UserProfileManager.userProfileList.get(UserProfileManager.currentProfileIndex)
-				.getDifficultyLevel().getConfidenceLevel().toString();
+  public void initialize() {
+    String[] currentDifficulties = new String[4];
+    currentDifficulties[0] =
+        UserProfileManager.userProfileList
+            .get(UserProfileManager.currentProfileIndex)
+            .getDifficultyLevel()
+            .getAccuracyLevel()
+            .toString();
+    currentDifficulties[1] =
+        UserProfileManager.userProfileList
+            .get(UserProfileManager.currentProfileIndex)
+            .getDifficultyLevel()
+            .getWordsLevel()
+            .toString();
+    currentDifficulties[2] =
+        UserProfileManager.userProfileList
+            .get(UserProfileManager.currentProfileIndex)
+            .getDifficultyLevel()
+            .getTimeLevel()
+            .toString();
+    currentDifficulties[3] =
+        UserProfileManager.userProfileList
+            .get(UserProfileManager.currentProfileIndex)
+            .getDifficultyLevel()
+            .getConfidenceLevel()
+            .toString();
 
-		Slider[] difficultySliders = new Slider[4];
-		difficultySliders[0] = this.accuracySlider;
-		difficultySliders[1] = this.wordsSlider;
-		difficultySliders[2] = this.timeSlider;
-		difficultySliders[3] = this.confidenceSlider;
+    Slider[] difficultySliders = new Slider[4];
+    difficultySliders[0] = this.accuracySlider;
+    difficultySliders[1] = this.wordsSlider;
+    difficultySliders[2] = this.timeSlider;
+    difficultySliders[3] = this.confidenceSlider;
 
-		for (int i = 0; i < 4; i++) {
-			if (currentDifficulties[i] == "E") {
-				difficultySliders[i].setValue((int) 0);
-			} else if (currentDifficulties[i] == "M") {
-				difficultySliders[i].setValue((int) 1);
-			} else if (currentDifficulties[i] == "H") {
-				difficultySliders[i].setValue((int) 2);
-			}
-		}
+    for (int i = 0; i < 4; i++) {
+      if (currentDifficulties[i] == "E") {
+        difficultySliders[i].setValue((int) 0);
+      } else if (currentDifficulties[i] == "M") {
+        difficultySliders[i].setValue((int) 1);
+      } else if (currentDifficulties[i] == "H") {
+        difficultySliders[i].setValue((int) 2);
+      }
+    }
 
-		for (int i = 0; i < 4; i++) {
-			difficultySliders[i].setLabelFormatter(new StringConverter<Double>() {
-				@Override
-				public String toString(Double number) {
-					if (number == 0) {
-						return "Easy";
-					} else if (number == 1) {
-						return "Medium";
-					} else {
-						return "Hard";
-					}
-				}
+    for (int i = 0; i < 4; i++) {
+      difficultySliders[i].setLabelFormatter(
+          new StringConverter<Double>() {
+            @Override
+            public String toString(Double number) {
+              if (number == 0) {
+                return "Easy";
+              } else if (number == 1) {
+                return "Medium";
+              } else {
+                return "Hard";
+              }
+            }
 
-				@Override
-				public Double fromString(String string) {
-					return null;
-				}
-			});
-		}
+            @Override
+            public Double fromString(String string) {
+              return null;
+            }
+          });
+    }
 
-		difficultySliders[0].valueProperty().addListener(new ChangeListener<Number>() {
-			@Override
-			public void changed(ObservableValue<? extends Number> observable, Number oldValue, Number newValue) {
-				if ((int) newValue == 0) {
-					updateUserAccuracyDifficulty(DifficultyLevel.Accuracy.E);
-				} else if ((int) newValue == 1) {
-					updateUserAccuracyDifficulty(DifficultyLevel.Accuracy.M);
-				} else {
-					updateUserAccuracyDifficulty(DifficultyLevel.Accuracy.H);
-				}
-			}
-		});
+    difficultySliders[0]
+        .valueProperty()
+        .addListener(
+            new ChangeListener<Number>() {
+              @Override
+              public void changed(
+                  ObservableValue<? extends Number> observable, Number oldValue, Number newValue) {
+                if ((double) newValue == 0) {
+                  updateUserAccuracyDifficulty(DifficultyLevel.Accuracy.E);
+                } else if ((double) newValue == 1) {
+                  updateUserAccuracyDifficulty(DifficultyLevel.Accuracy.M);
+                } else {
+                  updateUserAccuracyDifficulty(DifficultyLevel.Accuracy.H);
+                }
+              }
+            });
 
-		difficultySliders[1].valueProperty().addListener(new ChangeListener<Number>() {
-			@Override
-			public void changed(ObservableValue<? extends Number> observable, Number oldValue, Number newValue) {
-				if ((int) newValue == 0) {
-					updateUserWordsDifficulty(DifficultyLevel.Words.E);
-				} else if ((int) newValue == 1) {
-					updateUserWordsDifficulty(DifficultyLevel.Words.M);
-				} else {
-					updateUserWordsDifficulty(DifficultyLevel.Words.H);
-				}
-			}
-		});
+    difficultySliders[1]
+        .valueProperty()
+        .addListener(
+            new ChangeListener<Number>() {
+              @Override
+              public void changed(
+                  ObservableValue<? extends Number> observable, Number oldValue, Number newValue) {
+                if ((double) newValue == 0) {
+                  updateUserWordsDifficulty(DifficultyLevel.Words.E);
+                } else if ((double) newValue == 1) {
+                  updateUserWordsDifficulty(DifficultyLevel.Words.M);
+                } else {
+                  updateUserWordsDifficulty(DifficultyLevel.Words.H);
+                }
+              }
+            });
 
-		difficultySliders[0].valueProperty().addListener(new ChangeListener<Number>() {
-			@Override
-			public void changed(ObservableValue<? extends Number> observable, Number oldValue, Number newValue) {
-				if ((int) newValue == 0) {
-					updateUserTimeDifficulty(DifficultyLevel.Time.E);
-				} else if ((int) newValue == 1) {
-					updateUserTimeDifficulty(DifficultyLevel.Time.M);
-				} else {
-					updateUserTimeDifficulty(DifficultyLevel.Time.H);
-				}
-			}
-		});
+    difficultySliders[2]
+        .valueProperty()
+        .addListener(
+            new ChangeListener<Number>() {
+              @Override
+              public void changed(
+                  ObservableValue<? extends Number> observable, Number oldValue, Number newValue) {
+                if ((double) newValue == 0) {
+                  updateUserTimeDifficulty(DifficultyLevel.Time.E);
+                } else if ((double) newValue == 1) {
+                  updateUserTimeDifficulty(DifficultyLevel.Time.M);
+                } else {
+                  updateUserTimeDifficulty(DifficultyLevel.Time.H);
+                }
+              }
+            });
 
-		difficultySliders[0].valueProperty().addListener(new ChangeListener<Number>() {
-			@Override
-			public void changed(ObservableValue<? extends Number> observable, Number oldValue, Number newValue) {
-				if ((int) newValue == 0) {
-					updateUserConfidenceDifficulty(DifficultyLevel.Confidence.E);
-				} else if ((int) newValue == 1) {
-					updateUserConfidenceDifficulty(DifficultyLevel.Confidence.M);
-				} else {
-					updateUserConfidenceDifficulty(DifficultyLevel.Confidence.H);
-				}
-			}
-		});
-	}
+    difficultySliders[3]
+        .valueProperty()
+        .addListener(
+            new ChangeListener<Number>() {
+              @Override
+              public void changed(
+                  ObservableValue<? extends Number> observable, Number oldValue, Number newValue) {
+                if ((double) newValue == 0) {
+                  updateUserConfidenceDifficulty(DifficultyLevel.Confidence.E);
+                } else if ((double) newValue == 1) {
+                  updateUserConfidenceDifficulty(DifficultyLevel.Confidence.M);
+                } else {
+                  updateUserConfidenceDifficulty(DifficultyLevel.Confidence.H);
+                }
+              }
+            });
+  }
 
+  @FXML
+  private void onReturn(ActionEvent event) {
+    // Getting the scene information
+    Button button = (Button) event.getSource();
+    Scene sceneButtonIsIn = button.getScene();
+
+    // Changing the scene to waiting screen
+    try {
+      sceneButtonIsIn.setRoot(App.loadFxml("main_menu"));
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
+  }
 }
